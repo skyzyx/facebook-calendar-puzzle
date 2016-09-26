01d11c719e50a6ac343a94aa06e8a35911354fcd
Ryan Parman <ryan@ryanparman.com>
2016-09-26 03:08:00 -0700

    Tabs → Spaces.

--- util.js
+++ util.js
@@ -7,106 +7,106 @@
  */
 
 (function() {
-	/*jshint maxerr:false, smarttabs:true */
+    /*jshint maxerr:false, smarttabs:true */
 
-	"use strict";
+    "use strict";
 
-	// Init
-	var i, max, time, fmt,
-	d = document,
-	f = d.createDocumentFragment(),
+    // Init
+    var i, max, time, fmt,
+    d = document,
+    f = d.createDocumentFragment(),
 
-	// Render calendar events
-	renderEvent = function(evt) {
+    // Render calendar events
+    renderEvent = function(evt) {
 
-		var de = d.createElement('div'),
-		    dei = d.createElement('div'),
-		    h4 = d.createElement('h4'),
-		    p = d.createElement('p');
+        var de = d.createElement('div'),
+            dei = d.createElement('div'),
+            h4 = d.createElement('h4'),
+            p = d.createElement('p');
 
-		de.appendChild(dei);
-		dei.appendChild(h4);
-		dei.appendChild(p);
-		h4.appendChild(d.createTextNode('Sample Event #' + evt.id));
-		p.appendChild(d.createTextNode('Sample Location'));
+        de.appendChild(dei);
+        dei.appendChild(h4);
+        dei.appendChild(p);
+        h4.appendChild(d.createTextNode('Sample Event #' + evt.id));
+        p.appendChild(d.createTextNode('Sample Location'));
 
-		de.className = 'event';
-		dei.className = 'event-inner';
+        de.className = 'event';
+        dei.className = 'event-inner';
 
-		de.style.width = evt.width + 'px';
-		de.style.height = evt.height + 'px';
-		de.style.top = evt.top + 'px';
-		de.style.left = evt.left + 'px';
+        de.style.width = evt.width + 'px';
+        de.style.height = evt.height + 'px';
+        de.style.top = evt.top + 'px';
+        de.style.left = evt.left + 'px';
 
-		return de;
-	},
+        return de;
+    },
 
-	// Define range function with stepping
-	range = function(start, stop, step) {
-		var a = [start], b = start;
-		step = step || 1;
+    // Define range function with stepping
+    range = function(start, stop, step) {
+        var a = [start], b = start;
+        step = step || 1;
 
-		while (b < stop) {
-			b += step;
-			a.push(b);
-		}
+        while (b < stop) {
+            b += step;
+            a.push(b);
+        }
 
-		return a;
-	},
+        return a;
+    },
 
-	// Format time into human
-	fmtTime = function(seconds) {
-		var hour, mod, hh, md, mm;
+    // Format time into human
+    fmtTime = function(seconds) {
+        var hour, mod, hh, md, mm;
 
-		hh = (Math.floor(seconds / 60) + 9);
-		md = ((hh > 11) ? 'PM' : 'AM');
-		hh = ((hour = hh % 12) ? hour : 12);
-		mm = ((mod = seconds % 60) < 10 ? '0' + mod : mod);
+        hh = (Math.floor(seconds / 60) + 9);
+        md = ((hh > 11) ? 'PM' : 'AM');
+        hh = ((hour = hh % 12) ? hour : 12);
+        mm = ((mod = seconds % 60) < 10 ? '0' + mod : mod);
 
-		return [hh, mm, md];
-	},
+        return [hh, mm, md];
+    },
 
-	// Render time markers
-	fmtClock = function(fmt, offset, oclock) {
-		oclock = oclock || false;
+    // Render time markers
+    fmtClock = function(fmt, offset, oclock) {
+        oclock = oclock || false;
 
-		var di = d.createElement('div'),
-		    st = d.createElement('span'),
-		    sm = d.createElement('span');
+        var di = d.createElement('div'),
+            st = d.createElement('span'),
+            sm = d.createElement('span');
 
-		di.className = oclock ? 'oclock' : 'thirty';
-		di.style.top = offset + 'px';
-		st.className = 'time';
-		sm.className = 'meridian';
+        di.className = oclock ? 'oclock' : 'thirty';
+        di.style.top = offset + 'px';
+        st.className = 'time';
+        sm.className = 'meridian';
 
-		di.appendChild(st);
-		st.appendChild(d.createTextNode(fmt[0] + ':' + fmt[1]));
+        di.appendChild(st);
+        st.appendChild(d.createTextNode(fmt[0] + ':' + fmt[1]));
 
-		if (oclock) {
-			di.appendChild(d.createTextNode(' '));
-			di.appendChild(sm);
-			sm.appendChild(d.createTextNode(fmt[2]));
-		}
+        if (oclock) {
+            di.appendChild(d.createTextNode(' '));
+            di.appendChild(sm);
+            sm.appendChild(d.createTextNode(fmt[2]));
+        }
 
-		return di;
-	},
+        return di;
+    },
 
-	// Cache
-	steps = range(0, 720, 30);
+    // Cache
+    steps = range(0, 720, 30);
 
-	// Render the times
-	for (i = 0, max = steps.length; i < max; i++) {
-		time = steps[i];
-		fmt = fmtTime(time);
+    // Render the times
+    for (i = 0, max = steps.length; i < max; i++) {
+        time = steps[i];
+        fmt = fmtTime(time);
 
-		if (i % 2) {
-			f.appendChild(fmtClock(fmt, time, false));
-		}
-		else {
-			f.appendChild(fmtClock(fmt, time, true));
-		}
-	}
+        if (i % 2) {
+            f.appendChild(fmtClock(fmt, time, false));
+        }
+        else {
+            f.appendChild(fmtClock(fmt, time, true));
+        }
+    }
 
-	d.getElementById('time').appendChild(f);
-	window.renderEvent = renderEvent;
+    d.getElementById('time').appendChild(f);
+    window.renderEvent = renderEvent;
 })();
